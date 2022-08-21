81c9b9cfaf174d153de79ea3872aadcd3dc4369d
mezieOX <meziepage692@gmail.com>
2022-08-21 02:31:23 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="shadow-md h-12 flex items-center px-4">
-    <h1 className="text-purple-500 font-base">Hello World!!</h1>
+    <h1 className="text-purple-500 font-base">Hello mezie</h1>
     </div>
   )
 }
