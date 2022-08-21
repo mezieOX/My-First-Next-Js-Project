24fb68fb981c13ce2ab6435b447feaf4a222f94a
mezieOX <meziepage692@gmail.com>
2022-08-21 03:11:25 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+
+import { IconButton } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+
 export default function Home() {
   return (
-    <div className="shadow-md h-12 flex items-center px-4">
-    <h1 className="text-purple-500 font-base">Hello mezie</h1>
+    <div className="shadow-md h-12 flex items-center px-4 justify-between align-center">
+    <h1 className="text-purple-500 font-base">Hello World!!</h1>
+    <div>
+    <IconButton><MenuIcon /></IconButton>
+    </div>
     </div>
   )
 }
