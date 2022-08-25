375d67de12a747bfac6f68e5dc70820d5ef4da2c
mezieOX <meziepage692@gmail.com>
2022-08-25 10:01:29 -0700

    initial commit

--- components/sidebar/Sidebar.jsx
+++ components/sidebar/Sidebar.jsx
@@ -4,10 +4,10 @@ import CardIcon from '../icons/CardIcon';
 function Sidebar() {
   return (
     <div className="relative flex-[1.5] bg-[url('/images/bg-main-mobile.png')] bg-no-repeat bg-cover w-full">
-      <div className='absolute top-24 w-full text-right px-5'>
+      <div className='absolute top-5 w-full text-right px-5'>
         <Image src="/images/bg-card-back.png" width={300} height={150} alt="card-back" />
       </div>
-      <div className='absolute top-[230px] w-full h-28 text-center pr-5 pl-2'>
+      <div className='absolute top-[100px] w-full h-28 text-center pr-5 pl-2'>
         <Image src="/images/bg-card-front.png" className='shadow-xl' width={300} height={150} alt="card front"/>
       </div>
     </div>
